39b3ee5ba01fd5a5d3e344853cbc6908f2d8571c
AJROCKSTAR <ajrockstar1609@gmail.com>
2025-03-30 00:25:28 +0530

    Removed album cover feature

--- taylor_swift_app.py
+++ taylor_swift_app.py
@@ -65,50 +65,12 @@ csv_file = os.path.join(os.path.dirname(__file__), "songs_cleaned.csv")
 # Load the dataset
 df = pd.read_csv(csv_file)
 
-
 # Load saved TF-IDF model and vectorizer
 with open("tfidf_model.pkl", "rb") as model_file:
     tfidf_matrix = pickle.load(model_file)
 
 with open("tfidf_vectorizer.pkl", "rb") as vectorizer_file:
     vectorizer = pickle.load(vectorizer_file)
-
-# Manually mapped album cover paths
-album_covers = {
-    "1989 (Taylor's Version)": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/1989%20(Taylor's%20Version).png"  ,
-    "Beautiful Eyes": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Beautiful%20Eyes.png",
-    "Evermore": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Evermore.png",
-    "Fearless (Taylor's Version)": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Fearless%20(Taylor's%20Version).png",
-    "folklore": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/folklore.jpg",
-    "Lover": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Lover.jpg",
-    "Midnights": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Midnights.png",
-    "Red (Taylor's Version)": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Red%20(Taylor's%20Version).jpg",
-    "reputation": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/reputation.png",
-    "Speak Now (Taylor's Version)": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Speak%20Now%20(Taylor's%20Version).png",
-    "Taylor Swift": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Taylor%20Swift.png",
-    "The Tortured Poets Department": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/The%20Tortured%20Poets%20Department.jpg",
-    "The Hunger Games": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/artworks-000058368652-o8bupy-t500x500.jpg",
-    "How Long Do You Think It's Gonna Last": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/How_Long_Do_You_Think_It's_Gonna_Last_by_Big_Red_Machine.png",
-    "Cats": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/images%20(1).jpg",
-    "Where The Crawdads Sing": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Where_the_Crawdads_Sing_(film).jpg",
-    "Christmas Tree Farm": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/images%20(2).jpg",
-    "Fifty Shades Darker": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/MV5BMTQ5NTk0Njg2N15BMl5BanBnXkFtZTgwNzk5Nzk3MDI@._V1_.jpg",
-    "Miss Americana": r"https://raw.raw.githubusercontent.com/Abhishikt07/TaylorSwftLyricsSearch./refs/heads/main/album%20cover/AAAABRheY2bI3qqsOiRPUNuvGPgPEkXSFdWt1cH7v1VT4Icogjx4HAnPCHA0C8Of2YxTjH3CfJwX8Irrl1qzbP4JHh3jRjypMFQv9hOcs1gSha4yWOgveqxyw5U8LnGz1fmVDM1X%20(2).webp",
-    "Love Drunk": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/ab67616d0000b273597e14bb684692973a0843ba.jpg",
-    "Women in Music Part III": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Haim_-_Women_in_Music_Pt._III.png",
-    "Two Lanes of Freedom": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Twolanes.jpg",
-    "The Hannah Montana Movie": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/Hannah-montana-movie-poster.jpg",
-    "The Taylor Swift Holiday Collection": r"https://raw.github.com/Abhishikt07/TaylorSwiftLyricsSearch/album%20cover/images.jpg"
-}
-
-def get_album_cover(album_name):
-    album_image_path = album_covers.get(album_name)
-
-    if album_image_path:  # If image URL exists, return it
-        return album_image_path
-    
-    return album_covers.get(album_name, "https://via.placeholder.com/250x250/000000/FFFFFF?text=No+Image")
-
 
 
 # Function to search songs
@@ -124,16 +86,6 @@ def search_song(query, top_n=5):
 def get_full_lyrics(song_index):
     song = df.iloc[song_index]
     return {"title": song['Title'], "album": song['Album'], "lyrics": song['Lyrics']}
-
-def get_album_cover(album_name):
-    album_image_path = album_covers.get(album_name)
-
-    # Default black background if album cover is missing
-    if not album_image_path or not os.path.exists(album_image_path):
-        return r"D:\VS code\Taylore_swift\album cover\black_background.png"  # Black background image
-    
-    return album_image_path
-
 
 
 # ----------------------------------- Streamlit UI ---------------------------------------------
@@ -163,12 +115,6 @@ if query:
             # Find the index of selected song
             selected_index = results[results['Title'] == selected_title]['index'].values[0]
             song_details = get_full_lyrics(selected_index)
-
-            # Get album cover and dominant color
-            album_cover_path = get_album_cover(song_details['album'])
-
-            # Display album cover
-            st.image(album_cover_path, caption=f"{song_details['album']}", width=250)
 
         with col2:
             # Display full lyrics
